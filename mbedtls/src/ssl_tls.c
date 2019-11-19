e5b73b64a6558a06757af35ba3a28a7f3ab894f2
limingkun <mingkun.li@d3j.io>
2019-11-19 18:10:46 +0800

    Fix BUGs change PK cipher length handshake test succ

--- mbedtls/src/ssl_tls.c
+++ mbedtls/src/ssl_tls.c
@@ -6223,6 +6223,7 @@ static int ssl_append_key_cert( mbedtls_ssl_key_cert **head,
                                 mbedtls_x509_crt *cert,
                                 mbedtls_pk_context *key )
 {
+    int ret = 0;
     mbedtls_ssl_key_cert *new_cert;
 
     new_cert = mbedtls_calloc( 1, sizeof( mbedtls_ssl_key_cert ) );
@@ -6232,6 +6233,10 @@ static int ssl_append_key_cert( mbedtls_ssl_key_cert **head,
     new_cert->cert = cert;
     new_cert->key  = key;
     new_cert->next = NULL;
+    if( ( ret = mbedtls_pk_change_key_type( key,
+                    mbedtls_pk_get_type( &cert->pk ) ) ) ) {
+        return( ret );
+    }
 
     /* Update head is the list was null, else add to the end */
     if( *head == NULL )
